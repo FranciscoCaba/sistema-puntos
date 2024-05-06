9776d906b9569142aaf3b1eea1308f5bb2b3a11d
Derlis Cáceres <derliscaceresn@fpuna.edu.py>
2024-05-05 22:49:54 -0400

    Consulta GET clientes por nacionalidad y fecha

--- src/main/java/com/example/sistemapts/ejb/ClienteDAO.java
+++ src/main/java/com/example/sistemapts/ejb/ClienteDAO.java
@@ -1,13 +1,16 @@
 package com.example.sistemapts.ejb;
 
-import com.example.sistemapts.model.Cliente;
+import com.example.sistemapts.model.*;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.hibernate.Session;
 import org.hibernate.internal.SessionImpl;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Stateless
 public class ClienteDAO {
@@ -18,11 +21,42 @@ public class ClienteDAO {
         return (SessionImpl)entityManager.getDelegate();
     }
 
+    private boolean mailValido(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public void agregar(Cliente c) {
+        if (!mailValido(c.getEmail())) {
+            throw new IllegalArgumentException("El formato del correo electrónico no es válido");
+        }
         this.entityManager.persist(c);
     }
 
     public List<Cliente> obtenerPersonas() {
         return this.entityManager.createQuery("select c from Cliente c").getResultList();
+    }
+
+    public void eliminar (Integer idCliente) {
+        Cliente c=this.entityManager.find(Cliente.class,idCliente);
+        this.entityManager.remove(c);
+    }
+
+    public Cliente modificar (Cliente c) {
+        return this.entityManager.merge(c);
+    }
+
+    public List<Cliente> obtenerClientesPorNacionalidad(String nacionalidad) {
+        return entityManager.createQuery("SELECT c FROM Cliente c WHERE c.nacionalidad = :nacionalidad", Cliente.class)
+                .setParameter("nacionalidad", nacionalidad)
+                .getResultList();
+    }
+
+    public List<Cliente> obtenerClientesPorFechaNacimiento(LocalDate fechaNacimiento) {
+        return entityManager.createQuery("SELECT c FROM Cliente c WHERE c.fechaNacimiento = :fechaNacimiento", Cliente.class)
+                .setParameter("fechaNacimiento", fechaNacimiento)
+                .getResultList();
     }
 }
--- src/main/java/com/example/sistemapts/rest/ClienteRest.java
+++ src/main/java/com/example/sistemapts/rest/ClienteRest.java
@@ -7,6 +7,9 @@ import jakarta.ws.rs.*;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.core.Response;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Path("cliente")
 @Produces("application/json")
 @Consumes("application/json")
@@ -15,6 +18,12 @@ public class ClienteRest {
     @Inject
     ClienteDAO clienteDAO;
 
+    @GET
+    @Path("/")
+    public Response lista() {
+        return Response.ok(clienteDAO.obtenerPersonas()).build();
+    }
+
     @POST
     @Path("/")
     public Response agregar(Cliente c) {
@@ -22,9 +31,32 @@ public class ClienteRest {
         return Response.ok(c).build();
     }
 
-    @GET
-    @Path("/")
-    public Response lista() {
-        return Response.ok(clienteDAO.obtenerPersonas()).build();
+    @DELETE
+    @Path("/{id}")
+    public Response eliminar(@PathParam("id") Integer id) {
+        clienteDAO.eliminar(id);
+        return Response.ok().build();
     }
+
+    @PUT
+    @Path("/")
+    public Response modificar (Cliente c) {
+        Cliente p2=clienteDAO.modificar(c);
+        return Response.ok(p2).build();
+    }
+
+    @GET
+    @Path("/nacionalidad/{nacionalidad}")
+    public List<Cliente> obtenerClientesPorNacionalidad(@PathParam("nacionalidad") String nacionalidad) {
+        return clienteDAO.obtenerClientesPorNacionalidad(nacionalidad);
+    }
+
+    @GET
+    @Path("/fecha/{fecha}")
+    public List<Cliente> obtenerClientesPorFechaNacimiento(@PathParam("fecha") String fecha) {
+        LocalDate fechaNacimiento = LocalDate.parse(fecha);
+        return clienteDAO.obtenerClientesPorFechaNacimiento(fechaNacimiento);
+    }
+
+
 }
